e6907a004f92e052ecd505024d0334e61d83eca9
rushi9588-droid <rushi9588@gmail.com>
2026-08-19 18:22:46 +0530

    update

--- config.py
+++ config.py
@@ -1,11 +1,23 @@
-import mysql.connector
+# import os
+# import mysql.connector
 
+# connection = mysql.connector.connect(
+#     host=os.getenv("DB_HOST", "dpg-da2q8uj7uimc73b70vg0-a"),
+#     user=os.getenv("DB_USER", "shop_management_kxqv_user"),
+#     password=os.getenv("DB_PASSWORD", "cC3I6Iy9XiEvffVn28NSP3knYFhFgFdz"),
+#     database=os.getenv("DB_NAME", "shop_management_kxqv"),
+#     port=int(os.getenv("DB_PORT", 5432))
+# )
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="shop_management"
-)
-
-print("Database Connected Successfully")
+def get_connection():
+    connection = psycopg2.connect(
+        host=os.getenv("DB_HOST","dpg-da2q8uj7uimc73b70vg0-a"),
+        port=os.getenv("DB_PORT", 5432),
+        database=os.getenv("DB_NAME", "shop_management_kxqv"),
+        user=os.getenv("DB_USER", "shop_management_kxqv_user"),
+        password=os.getenv("DB_PASSWORD", "cC3I6Iy9XiEvffVn28NSP3knYFhFgFdz")
+    )
+    return connection
